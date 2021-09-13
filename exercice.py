a98379792ce4a1a0355781f249eb1b86f407ee6b
mHammache <79108709+mHammache@users.noreply.github.com>
2021-09-13 14:31:31 -0400

    Solutions for every exercise

--- exercice.py
+++ exercice.py
@@ -3,23 +3,43 @@
 
 
 def is_even_len(string: str) -> bool:
-    return False
+    even_len = None
+    if (len(string) %2 == 0):
+        even_len = True
+    else:
+        even_len = False
+    return even_len
 
 
 def remove_third_char(string: str) -> str:
-    return ""
+    to_list = list(string)
+    to_list[2] = ""
+    new_string = ""
+    for letter in to_list:
+        new_string += letter
+    return new_string
 
 
 def replace_char(string: str, old_char: str, new_char: str) -> str:
-    return ""
+    string = string.replace(old_char, new_char)
+    return string
 
 
 def get_nb_char(string: str, char: str) -> int:
-    return 0
+    nb_char = 0
+    for letter in string:
+        if (letter == char):
+            nb_char +=1
+    return nb_char
 
 
 def get_nb_words(sentence: str) -> int:
-    return 0
+    nb_words = 1
+    for letter in sentence:
+        if(letter == " "):
+            nb_words += 1
+
+    return nb_words
 
 
 def main() -> None:
